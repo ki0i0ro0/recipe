99f3ebaf71c1630ef46d6e7ee657a647ced64285
ki0i0ro0 <23109879+ki0i0ro0@users.noreply.github.com>
2022-08-17 23:34:47 +0900

    [add] auth0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 const client = new ApolloClient({
   uri: '/api/graphql',
@@ -9,9 +9,11 @@ const client = new ApolloClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </UserProvider>
   )
 }
 
