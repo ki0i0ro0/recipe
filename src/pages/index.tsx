315d8441e30ea3c2fedf3e2118151dce8583cb66
ki0i0ro0 <23109879+ki0i0ro0@users.noreply.github.com>
2022-08-17 23:40:01 +0900

    [update] shown list items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import { EnhancedTable } from '@/components/EnhancedTable'
-import type { Recipe } from '@/types'
-import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { AddCircle } from '@mui/icons-material'
-import { CircularProgress, IconButton } from '@mui/material'
-import Router from 'next/router'
-import { useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
+import { withPageAuthRequired, useUser } from '@auth0/nextjs-auth0'
+import { AddCircle } from '@mui/icons-material'
+import {
+  CircularProgress,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material'
+import Router from 'next/router'
 
 const GET_RECIPE = gql`
   query getRecipe($email: String!) {
@@ -17,11 +23,10 @@ const GET_RECIPE = gql`
 `
 
 const App = () => {
-  const [selected, setSelected] = useState<readonly string[]>([])
   const { user } = useUser()
-  const { data, loading, error } = useQuery<any[]>(GET_RECIPE, {
+  const { data, loading, error } = useQuery<any>(GET_RECIPE, {
     variables: {
-      email: user!.email,
+      email: user?.email,
     },
   })
 
@@ -29,15 +34,10 @@ const App = () => {
   if (error) return <p>エラーが発生しています</p>
   if (!data) return <CircularProgress />
 
+  const rows: any[] = JSON.parse(data.recipe.data)
   const handleCreate = () => {
     Router.push({ pathname: `/create` })
   }
-
-  const handleEdit = (id: number) => {
-    Router.push({ pathname: `/update/${id}` })
-  }
-
-  const rowDeleteClicked = (ids: string[]) => {}
 
   return (
     <>
@@ -49,13 +49,26 @@ const App = () => {
         <AddCircle fontSize="large" />
       </IconButton>
 
-      <EnhancedTable
-        rawRows={data}
-        handleEdit={handleEdit}
-        rowDeleteClicked={rowDeleteClicked}
-        selected={selected}
-        setSelected={setSelected}
-      />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>料理名</TableCell>
+              <TableCell>作成日</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => {
+              return (
+                <TableRow onClick={(event) => console.log(event)} key={row.name}>
+                  <TableCell>{row.name || 'N/A'}</TableCell>
+                  <TableCell>{new Date(row.updatedAt).toLocaleDateString()}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
