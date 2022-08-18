afd3aa76f5ada8cbe621c7433a51cea0ef10ae57
ki0i0ro0 <23109879+ki0i0ro0@users.noreply.github.com>
2022-08-18 22:14:23 +0900

    [remove] default Value

--- src/utils/create-values.ts
+++
@@ -1,7 +0,0 @@
-import { defaultValue } from '../config'
-import { Recipe } from '../types'
-
-export const createValues = (values?: Recipe): Recipe => {
-  if (!values) return { ...defaultValue }
-  return { ...values }
-}
