9d458dd7871da1e03572b27168694b2f8c605494
ki0i0ro0 <ki0i0ro0@gmail.com>
2022-09-02 22:06:11 +0900

    [update] the db converts to firestore

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,6 +1,20 @@
 import { PrismaClient } from '@prisma/client'
 import { ApolloServer, gql } from 'apollo-server-micro'
+import * as admin from 'firebase-admin'
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+if (admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      // cert()の中に直接JSON形式で代入
+      projectId: process.env.FSA_PROJECT_ID,
+      privateKey: (process.env.FSA_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+      clientEmail: process.env.FSA_CLIENT_EMAIL,
+    }),
+  })
+}
+
+const db = admin.firestore()
 
 const prisma = new PrismaClient()
 
@@ -33,39 +47,50 @@ const typeDefs = gql`
 `
 
 /**
- * ユーザー毎のレシピ取得
+ * Get user's id by email address
+ * @param email
+ * @returns
+ */
+const getUserId = async (email: string) => {
+  const userRef = db.collection('users').doc(email)
+  const doc = await userRef.get()
+  return doc.exists ? doc.data()?.id : 0
+}
+
+/**
+ * Get user recipe list by email address
  * @param args
  * @returns
  */
 const getRecipe = async (args: { email: string }) => {
-  const { email } = args
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  })
+  const userId = await getUserId(args.email)
   return prisma.recipe.findMany({
     where: {
-      user_id: user?.id,
+      user_id: userId,
     },
   })
 }
 
+/**
+ * Create user related menu
+ * @param args
+ * @returns
+ */
 const addUserRecipe = async (args: { email: string; menuId: number }) => {
-  const { email } = args
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  })
+  const userId = await getUserId(args.email)
   return prisma.recipe.create({
     data: {
-      user_id: user?.id ?? 0,
+      user_id: userId,
       menu_id: args.menuId,
     },
   })
 }
 
+/**
+ * Delete user's one recipe
+ * @param args
+ * @returns
+ */
 const removeUserRecipe = async (args: { recipeId: number }) => {
   const { recipeId } = args
   return prisma.recipe.delete({
@@ -75,49 +100,72 @@ const removeUserRecipe = async (args: { recipeId: number }) => {
   })
 }
 
+/**
+ * Delete user's all recipe
+ * @param args
+ * @returns
+ */
 const resetUserRecipe = async (args: { email: string }) => {
-  const { email } = args
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  })
+  const userId = await getUserId(args.email)
   return prisma.recipe.deleteMany({
     where: {
-      user_id: user?.id,
+      user_id: userId,
     },
   })
 }
 
 /**
- * メニュー作成
+ * Create Menu
  * @param args
  * @returns
  */
-const createMenu = (args: { name: string; url?: string }) =>
-  prisma.menu.create({
-    data: {
-      name: args.name,
-      url: args.url || '',
-    },
-  })
+const createMenu = async (args: { name: string; url?: string }) => {
+  const { name, url } = args
+  const idNum = +new Date()
+  const id = idNum.toString().slice(0, -3)
+  const docRef = db.collection('menus').doc(id)
+  const data = {
+    id: +id,
+    name,
+    url: url || '',
+  }
+  await docRef.set(data)
+  return data
+}
 
-const updateMenu = (args: { id: number; name: string; url: string }) =>
-  prisma.menu.update({
-    where: {
-      id: args.id,
-    },
-    data: {
-      name: args.name,
-      url: args.url,
-    },
-  })
+/**
+ * Update Menu
+ * @param args
+ * @returns
+ */
+const updateMenu = async (args: { id: number; name: string; url: string }) => {
+  const { id, name, url } = args
+  const docRef = db.collection('menus').doc(id.toString())
+  const data = {
+    id,
+    name,
+    url: url || '',
+  }
+  await docRef.set(data)
+  return data
+}
+
+/**
+ * Get All Menu
+ * @returns
+ */
+const getMenus = async () => {
+  const menus = await db.collection('menus').get()
+  const buff: { id: number; name: string; url: string }[] = []
+  menus.forEach((menu) => buff.push(menu.data() as { id: number; name: string; url: string }))
+  return buff
+}
 
 const resolvers = {
   Query: {
     recipe: (_: undefined, args: any) => getRecipe(args),
     menu: (_: undefined, args: any) => prisma.menu.findFirst({ where: { id: args.id } }),
-    menus: () => prisma.menu.findMany(),
+    menus: () => getMenus(),
   },
   Mutation: {
     addUserRecipe: (_: undefined, args: any) => addUserRecipe(args),
