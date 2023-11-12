680ae60fe84b9f37c70c23d4ec4d448a82201aad
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-11-12 22:25:52 +0900

    TOPをserver componentに変更

--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { AutoMode, MenuBook } from "@mui/icons-material";
 import {
-  IconButton,
   Table,
   TableBody,
   TableCell,
@@ -9,63 +6,28 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import { BaseDrawer } from "@/components/BaseDrawer";
-import { BaseLoading } from "@/components/BaseLoading";
-import { userMenuState } from "@/stores/userMenu";
+import Link from "next/link";
 import type { AppMenu, GetRecipe } from "@/types";
 
 export const TopPage = ({ data }: { data: GetRecipe }) => {
-  const router = useRouter();
-  const [rows, setRows] = useState<AppMenu[]>([]);
-
-  useEffect(() => {
-    if (data) {
-      const cookedMenus: AppMenu[] = data?.menus.map((menu) => {
-        const cookedMenu = data?.recipe.find(
-          (userMenu) => +userMenu.menuId === +menu.id,
-        );
-        return {
-          menuId: menu.id,
-          menuName: menu.name,
-          url: menu.url,
-          createdAt: cookedMenu?.createdAt || "",
-          recipeId: cookedMenu?.id ?? 0,
-        };
-      });
-      cookedMenus.sort((a, b) => (a.createdAt < b.createdAt ? -1 : 1));
-      setRows(cookedMenus || []);
-    }
-  }, [data]);
-
-  if (!data) return <BaseLoading />;
-
-  const handleUpdate = (row: AppMenu) => {
-    userMenuState(row);
-    router.push("/recipe/update");
-  };
-
-  const handleRecipeURL = (url?: string) => {
-    if (url && url.length > 5) {
-      window.open(url);
-    }
-  };
+  const cookedMenus: AppMenu[] = data?.menus.map((menu) => {
+    const cookedMenu = data?.recipe.find(
+      (userMenu) => +userMenu.menuId === +menu.id,
+    );
+    return {
+      menuId: menu.id,
+      menuName: menu.name,
+      url: menu.url,
+      createdAt: cookedMenu?.createdAt || "",
+      recipeId: cookedMenu?.id ?? 0,
+    };
+  });
+  cookedMenus.sort((a, b) => (a.createdAt < b.createdAt ? -1 : 1));
+  const rows = cookedMenus || [];
 
   return (
     <BaseDrawer>
-      {/* Menu add Button */}
-      <IconButton
-        sx={{ position: "fixed", bottom: 16, right: 16 }}
-        color="info"
-        onClick={() => {
-          router.push("/recipe/decide");
-        }}
-      >
-        <AutoMode fontSize="large" />
-      </IconButton>
-
-      {/* Menu List */}
       <TableContainer>
         <Table>
           <TableHead>
@@ -78,20 +40,25 @@ export const TopPage = ({ data }: { data: GetRecipe }) => {
           <TableBody>
             {rows.map((row) => {
               return (
-                <TableRow
-                  onClick={(event) => console.log(event)}
-                  key={row.menuId}
-                >
-                  <TableCell onClick={() => handleUpdate(row)}>
-                    {row.menuName}
+                <TableRow key={row.menuId}>
+                  <TableCell>
+                    <Link
+                      href={`/recipe/update?menuId=${row.menuId}&recipeId=${row.recipeId}&createdAt=${row.createdAt}&menuName=${row.menuName}`}
+                    >
+                      {row.menuName}
+                    </Link>
                   </TableCell>
                   <TableCell>
                     {row.createdAt
                       ? new Date(row.createdAt).toLocaleDateString()
                       : null}
                   </TableCell>
-                  <TableCell onClick={() => handleRecipeURL(row.url)}>
-                    {row.url && row.url.length > 5 ? <MenuBook /> : null}
+                  <TableCell>
+                    {row.url && row.url.length > 5 ? (
+                      <a href={row.url} target="_blank" title="レシピリンク">
+                        レシピ
+                      </a>
+                    ) : null}
                   </TableCell>
                 </TableRow>
               );
