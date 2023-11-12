b9f490c57bc840bbd5238f601b3df5b03421e7cd
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-11-12 22:19:15 +0900

    レシピのルーレットをserver actionsに変更

--- src/app/recipe/decide/page.tsx
+++ src/app/recipe/decide/page.tsx
@@ -1,30 +1,16 @@
-"use client";
-import { useMutation, useQuery } from "@apollo/client";
 import { AutoMode } from "@mui/icons-material";
-import { LoadingButton } from "@mui/lab";
-import { Avatar, Box, Button, Typography } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import { BaseLoading } from "@/components/BaseLoading";
+import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
 import { BasePage } from "@/components/BasePage";
-import { ADD_USER_RECIPE } from "@/graphql/recipe/add";
-import { GET_RECIPE } from "@/graphql/recipe/get";
-import type { AppMenu, GetRecipe, Menu } from "@/types";
+import type { AppMenu, Menu } from "@/types";
+import { getCookedMenu, handleAddUserRecipe } from "@/app/actions";
+import Link from "next/link";
 
-const App = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const [addUserRecipe] = useMutation(ADD_USER_RECIPE);
-  const [menu, setMenu] = useState<Menu>();
-  const [unCookedMenus, setUnCookedMenus] = useState<AppMenu[]>([]);
-  const { data, loading, error } = useQuery<GetRecipe>(GET_RECIPE, {
-    variables: {
-      email: session?.user?.email,
-    },
-  });
+const App = async () => {
+  let menu: Menu;
+  const cookedMenus = await getCookedMenu();
 
-  const ShuffleMenu = (unCookedMenus: AppMenu[]) => {
+  const ShuffleMenu = (cookedMenus: AppMenu[]) => {
+    const unCookedMenus = cookedMenus.filter((v) => !v.createdAt);
     if (unCookedMenus.length < 1) return;
     const todaysAppMenu =
       unCookedMenus[Math.floor(Math.random() * unCookedMenus.length)];
@@ -33,41 +19,11 @@ const App = () => {
       name: todaysAppMenu.menuName,
       url: "",
     };
-    setMenu(todaysMenu);
+    menu = todaysMenu;
   };
 
-  useEffect(() => {
-    if (!data) return;
-    const cookedMenus: AppMenu[] = data?.menus.map((menu) => {
-      const cookedMenu = data?.recipe.find(
-        (userMenu) => userMenu.menuId === menu.id,
-      );
-      return {
-        menuId: menu.id ?? 0,
-        menuName: menu.name,
-        createdAt: cookedMenu?.createdAt || "",
-        recipeId: cookedMenu?.id ?? 0,
-      };
-    });
-
-    const unCookedMenus = cookedMenus.filter((v) => !v.createdAt);
-    setUnCookedMenus(unCookedMenus);
-    ShuffleMenu(unCookedMenus);
-  }, [data]);
-
-  if (loading) return <BaseLoading />;
-  if (error) return <p>エラーが発生しています</p>;
-  if (!data) return <BaseLoading />;
-
-  const handleUpdate = async (id: number) => {
-    await addUserRecipe({
-      variables: {
-        email: session?.user?.email,
-        menuId: id,
-      },
-    });
-    router.push("/");
-  };
+  const unCookedMenus = cookedMenus.filter((v) => !v.createdAt);
+  ShuffleMenu(unCookedMenus);
 
   return (
     <BasePage>
@@ -76,29 +32,24 @@ const App = () => {
       </Avatar>
       <Box>
         <Typography color="text.secondary" gutterBottom>
-          本日のおすすめメニューは
+          本日のおすすめメニュー
         </Typography>
         <Typography variant="h5" component="div" gutterBottom>
-          {menu?.name}
+          {menu!.name}
         </Typography>
         <Typography color="text.secondary" gutterBottom>
           登録しますか？
         </Typography>
-        <LoadingButton
-          onClick={() => {
-            handleUpdate(menu?.id ? +menu?.id : 0);
-          }}
-        >
-          OK
-        </LoadingButton>
-        <Button
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          Cancel
-        </Button>
-        <Button onClick={() => ShuffleMenu(unCookedMenus)}>Retry</Button>
+        <form action={handleAddUserRecipe}>
+          <input type="hidden" name="menuId" value={menu!.id} />
+          <Stack>
+            <Button type="submit">はい</Button>
+            <Button href="/recipe/decide">やりなおす</Button>
+            <Button>
+              <Link href="/">戻る</Link>
+            </Button>
+          </Stack>
+        </form>
       </Box>
     </BasePage>
   );
