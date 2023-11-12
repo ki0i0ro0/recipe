87d9b88b4102cfdbd9379ecacf20452606b477e8
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-11-12 22:52:27 +0900

    layoutをserver componentに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
-"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "recipe",
-//   description: "random recipe app",
-// };
+export const metadata: Metadata = {
+  title: "recipe",
+  description: "random recipe app",
+};
 
 export default function RootLayout({
   children,
@@ -23,9 +21,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" type="image/png" href="favicon.png" />
         <meta name="theme-color" content="#fff" />
       </head>
-      <body className={inter.className}>
-        <SessionProvider>{children}</SessionProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
