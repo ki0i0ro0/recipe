603cc2f3391c0b947308d48c2250782161e90a2b
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-11-12 22:23:51 +0900

    パスパラメーターに変更したのでストアを削除

--- src/stores/userMenu.ts
+++
@@ -1,9 +0,0 @@
-import { makeVar } from "@apollo/client";
-import type { AppMenu } from "@/types";
-// 現在のログイン情報を保持するリアクティブ変数。引数に初期値(false)を指定します。
-export const userMenuState = makeVar({
-  menuId: 0,
-  menuName: "",
-  createdAt: "",
-  recipeId: 0,
-} as AppMenu);
