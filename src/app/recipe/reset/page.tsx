5d41523b627c82cbb26d923f68774527f075caf3
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2023-11-14 13:30:03 +0900

    レシピリセット画面追加

---
+++ src/app/recipe/reset/page.tsx
@@ -0,0 +1,29 @@
+import { PlaylistRemove } from "@mui/icons-material";
+import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
+import { BasePage } from "@/components/BasePage";
+import { handleDeleteUserRecipes } from "@/app/actions";
+import { ReturnButton } from "@/components/ReturnButton";
+
+export default function Page() {
+  return (
+    <BasePage>
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <PlaylistRemove />
+      </Avatar>
+      <Typography color="text.secondary">レシピリセット</Typography>
+      <Box sx={{ paddingTop: "1em" }}>
+        <Typography color="text.secondary" gutterBottom paddingBottom={2}>
+          レシピの作成履歴をリセットしますか？
+        </Typography>
+        <form action={handleDeleteUserRecipes}>
+          <Stack gap={1}>
+            <Button type="submit" variant="contained">
+              リセット
+            </Button>
+            <ReturnButton />
+          </Stack>
+        </form>
+      </Box>
+    </BasePage>
+  );
+}
